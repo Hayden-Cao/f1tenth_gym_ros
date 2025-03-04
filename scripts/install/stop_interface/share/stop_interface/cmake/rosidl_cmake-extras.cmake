8401b54ded69d5d4fe359d62baa12b41cfc9f5a9
Hayden <haydencao1@gmail.com>
2025-03-04 00:11:35 -0800

    3/4/2025

--- scripts/install/stop_interface/share/stop_interface/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(stop_interface_IDL_FILES "msg/StopData.idl;srv/StopOrGo.idl")
-set(stop_interface_INTERFACE_FILES "msg/StopData.msg;srv/StopOrGo.srv;srv/StopOrGo_Request.msg;srv/StopOrGo_Response.msg")
--- scripts/install/stop_interfaces/share/stop_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(stop_interfaces_IDL_FILES "action/StopOrGo.idl")
-set(stop_interfaces_INTERFACE_FILES "action/StopOrGo.action")
